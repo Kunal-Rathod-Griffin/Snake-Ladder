a7f9e0040be57b5968617e0202e95054e1d5b313
Rathod Kunal <86817361+Kunal-Rathod-Griffin@users.noreply.github.com>
2021-12-18 21:38:12 +0530

    Add files via upload

---
+++ SnL/src/main/java/com/example/snl/HelloController.java
@@ -0,0 +1,54 @@
+package com.example.snl;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+
+public class HelloController {
+
+
+    @FXML
+    private Button Dice;
+
+    @FXML
+    private ImageView diceImage;
+
+    @FXML
+    private ImageView gameBoard;
+
+    @FXML
+    private GridPane gameGrid;
+
+    @FXML
+    private Pane scene1;
+
+    public void tester()
+    {
+
+    }
+
+}
+
+class Player{
+
+}
+
+class Piece{
+
+}
+
+class Snake{
+
+}
+
+class Ladder{
+
+
+}
+
+class Dice{
+
+}
+
