056f9d75f897ccbceeb519936f2c810a23e1587a
Rathod Kunal <86817361+Kunal-Rathod-Griffin@users.noreply.github.com>
2021-12-22 11:30:17 +0530

    Add files via upload

--- SnL/src/main/java/com/example/snl/HelloApplication.java
+++ SnL/src/main/java/com/example/snl/HelloApplication.java
@@ -6,19 +6,25 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.HashMap;
 
 public class HelloApplication extends Application {
     @Override
 
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 550, 700);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 563, 700);
+        stage.setTitle("Ludo Master");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+
+
         System.getProperty("javafx.runtime.version");
         launch();
     }
